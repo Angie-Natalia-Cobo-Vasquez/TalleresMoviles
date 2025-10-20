336744c33b39e25c78c0d41fc4fd6a118c4de6a6
NatyVasquez <148178721+NatyVasquez@users.noreply.github.com>
2025-10-20 00:47:37 -0500

    feat: integración de Firebase App Distribution con release 1.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // ✅ Activa Firebase Google Services
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,20 +22,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.talleresmoviles"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+
+        // Versionado semántico
+        versionCode = 1000001
+        versionName = "1.0.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +40,13 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// ========== DEPENDENCIAS DE FIREBASE ==========
+dependencies {
+    // Firebase BoM (Bill of Materials)
+   implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+
+    // Firebase Core + Analytics (recomendadas por defecto)
+    implementation("com.google.firebase:firebase-analytics")
 }
